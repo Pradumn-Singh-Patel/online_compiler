820c8d265cdacdcec7bf676390df443bc92886fe
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2024-04-16 01:37:43 +0530

    resolving export error after deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials:true,
-  origin: "http://localhost:5173"
+  origin: "https://online-compiler-ui.vercel.app"
 }));
 config();
 
