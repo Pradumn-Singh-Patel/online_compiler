e9a77594d8902107e86135720c0dbaafa3b2079a
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2024-04-17 01:34:13 +0530

    resolving deployment error

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials:true,
-  origin: "https://online-compiler-ui.vercel.app"
+  // origin: "https://online-compiler-ui.vercel.app"
+  origin:"http://localhost:5173"
 }));
 config();
 
